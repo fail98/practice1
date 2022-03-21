49f45eb272a4c53a12187988bf2c28e752e1f49e
fail98 <fail98@bk.ru>
2022-03-21 13:10:51 +0300

    Practice 1

---
+++ clickbuttons/src/main/java/ru/mirea/abdullin/clickbuttons/MainActivity.java
@@ -0,0 +1,38 @@
+package ru.mirea.abdullin.clickbuttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvOut;
+    private Button buttonOk;
+    private Button buttonCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        buttonOk = (Button) findViewById(R.id.btnOk);
+        buttonCancel = (Button) findViewById(R.id.btnCancel);
+
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvOut.setText("Нажата кнопка ОК");
+            }
+        };
+        buttonOk.setOnClickListener(oclBtnOk);
+    }
+    public void onMyButtonClick(View view)
+    {
+        // выводим сообщение
+        Toast.makeText(this, "Ещё один способ!", Toast.LENGTH_SHORT).show();
+    }
+}
